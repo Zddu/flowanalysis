c2dfb639e9dc444a84ba6fe37bedb6b2b8ad90f0
Administrator <qh17992659860@163.com>
2020-05-08 07:42:07 +0800

    Merge remote-tracking branch 'remotes/origin/master' into customer
    # Conflicts:
    #	src/main/java/com/cidp/monitorsystem/service/dispservice/DiagnosisService.java
    #	src/main/java/com/cidp/monitorsystem/util/GetSpecialString.java

--- src/main/java/com/cidp/monitorsystem/util/GetSpecialString.java
+++ src/main/java/com/cidp/monitorsystem/util/GetSpecialString.java
@@ -1,11 +1,13 @@
 package com.cidp.monitorsystem.util;
 
+import java.util.List;
+
 /**
  * @date 2020/5/7 -- 16:47
  **/
 public class GetSpecialString {
     //得到用"，"分隔的字符串
-    public static String getCommaSeparated(String[] strings){
+    public static String getCommaSeparated(List<String> strings){
         String ids="";
         for (String s : strings) {
             ids+=s+",";
