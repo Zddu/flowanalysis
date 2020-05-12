4d68a590685c22d2f131fa23ade91a46619148a1
Zddu <1501815524@qq.com>
2020-05-12 19:06:43 +0800

    v1.0

--- src/main/java/com/cidp/monitorsystem/model/SeriesData.java
+++ src/main/java/com/cidp/monitorsystem/model/SeriesData.java
@@ -4,11 +4,9 @@ import lombok.Data;
 
 /**
  * echarts 图表插件 中饼状图的data数据格式
- * system表 各类型的数量
- * 二层交换机, 三层交换机, 路由器, 服务器
  */
 @Data
 public class SeriesData {
-    private Integer value; //设备数量
-    private String name;//设备名称
+    private Integer value; //设备数量/监测点故障次数
+    private String name;//设备名称/监测点故障名称
 }
