964b67a9fd168fc55ce97772809c83cf06f140cc
Administrator <qh17992659860@163.com>
2020-06-02 19:16:46 +0800

    Merge remote-tracking branch 'remotes/origin/master' into customer
    # Conflicts:
    #	src/main/java/com/cidp/monitorsystem/service/dispservice/DiagnosisService.java
    #	src/main/java/com/cidp/monitorsystem/util/GetSpecialString.java

--- src/main/java/com/cidp/monitorsystem/ml/Start.java
+++ src/main/java/com/cidp/monitorsystem/ml/Start.java
@@ -5,6 +5,6 @@ import com.cidp.monitorsystem.ml.convert.PcapReader;
 
 public class Start {
     public static void main(String[] args) {
-        PcapReader.readFile("C:\\Users\\Administrator\\Desktop\\1.pcap","C:\\Users\\Administrator\\Desktop",120000000L,5000000L);
+        PcapReader.readFile("C:\\Users\\Administrator\\Desktop\\1\\a.pcap","C:\\Users\\Administrator\\Desktop\\1",120000000L,5000000L);
     }
 }
